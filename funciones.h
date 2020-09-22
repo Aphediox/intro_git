06444af90bee0d5213eda21451c335750cd934f9
Salvador Torrres <jorge.torres4847@alumnos.udg.mx>
2020-09-22 11:47:02 -0500

    Las funciones thegame e iteraciones ya estan hechas

--- funciones.h
+++ funciones.h
@@ -1,13 +1,16 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
- 
+#include <math.h>
+#include <stdio.h> 
 float iteraciones(float a, float b){
-
-
+    float f,n;
+    f=(b-a)/0.00001;
+    n=log(f)/log(2);
+    return n;
 }
 
 void thegame(){
-    
+    printf("Haz perdido The Game.");
 }
 
 #endif FUNCIONES_H
